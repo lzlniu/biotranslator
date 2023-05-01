bcfd63be4ce0b17b727cf903db876173dd8d05d2
ZELL <zell@novonordisk.com>
2023-05-01 16:28:47 +0200

    package name issues

--- biotranslator/__init__.py
+++ biotranslator/__init__.py
@@ -1,7 +1,7 @@
 """BioTranslator in Python."""
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
-from . import biotranslator as bt
+from . import BioTranslator as bt
 from . import config, loader, text_encoder, trainer, utils
 
 from .biotranslator_function import setup_config, train_text_encoder, train_biotranslator, \
